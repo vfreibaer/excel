42edd66d52b40c365859423206bd1b751b4182aa
Vitus <Vitus@vitus.is2.de>
2018-06-27 20:23:10 +0200

    bbb

--- src/main/java/de/freibaer/excel/PoijRow.java
+++ src/main/java/de/freibaer/excel/PoijRow.java
@@ -9,43 +9,30 @@ import lombok.Setter;
 @Getter
 @Setter
 public final class PoijRow extends AbstractRow {
-	// Col_A: Vertriebsname
 	@ExcelCell(0)
 	private String colA;
-	// Col_B: Hauptversicherung
 	@ExcelCell(1)
 	private String colB;
-	// Col_C: Tarif HV
 	@ExcelCell(2)
 	private String colC;
-	// Col_D: Fondsklasse
 	@ExcelCell(3)
 	private String colD;
-	// Col_E: Produktmerkmale
 	@ExcelCell(4)
 	private String colE;
-	// Col_F: Zusatzversicherung
 	@ExcelCell(5)
 	private String colF;
-	// Col_G: Tarif NV
 	@ExcelCell(6)
 	private String colG;
-	// Col_H: Abschluss-Alter HV min.
 	@ExcelCell(7)
 	private String colH;
-	// Col_I: Risikoklasse, Laufzeit ab 12 Jahre
 	@ExcelCell(8)
 	private String colI;
-	// Col_J: Risikoklasse, Laufzeit unter 12 Jahre
 	@ExcelCell(9)
 	private String colJ;
-	// Col_K: Absicherung des Einkommensverlustes: Sehr wichtig, wichtig, neutral
 	@ExcelCell(10)
 	private String colK;
-	// Col_L: regelmaesige Beitragszahlung
 	@ExcelCell(11)
 	private String colL;
-	// Col_M: Einmalzahlung
 	@ExcelCell(12)
 	private String colM;
 	@ExcelCell(13)
